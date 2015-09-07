d778bb4f05f9badb39f61dc4718f75ed5a9ed28a
yanjiabao <yanjiabao@jd.com>
2015-09-07 12:58:23 +0800

    next proxy

--- labs-rpc/src/main/java/com/juaby/labs/rpc/proxy/RpcAnnotation.java
+++ labs-rpc/src/main/java/com/juaby/labs/rpc/proxy/RpcAnnotation.java
@@ -3,9 +3,9 @@ package com.juaby.labs.rpc.proxy;
 /**
  * Title: <br>
  * Description: <br>
- * Company: <a href=www.jd.com>京东</a> <br>
+ * Company: <a href=www.juaby.com>卓贝</a> <br>
  *
- * @author <a href=mailto:yanjiabao@jd.com>yanjiabao</a> <br>
+ * @author <a href=mailto:yanjiabao@juaby.com>yanjiabao</a> <br>
  * @date Created by yanjiabao on 2015/9/2 9:49.
  */
 public @interface RpcAnnotation {
